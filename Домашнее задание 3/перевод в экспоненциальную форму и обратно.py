3eec7c0710ed6c7e5ce4bbdbeb7e9ca518c2e093
KristinaChervaneva <chervaneva.kristina@gmail.com>
2022-10-08 16:21:21 +0300

    перевод

--- Домашнее задание 3/перевод в экспоненциальную форму и обратно.py
+++ Домашнее задание 3/перевод в экспоненциальную форму и обратно.py
@@ -4,10 +4,12 @@ class ComplexNumber:
 
     def __init__(self, my_a = 0, my_b = 0): #инициализатор
         self.set(my_a, my_b)
+        return
 
     def set(self, my_a, my_b): #сеттер
         self.a = my_a
         self.b = my_b
+        return
 
     def get(self): #геттер
         return self.a, self.b
@@ -17,5 +19,12 @@ class ComplexNumber:
         return
 
     def ExpToAlgebra(self):
-        self.a, self.b =
+        self.a, self.b = self.a * math.cos(self.b), self.a * math.sin(self.b)
+        return
+
+z = ComplexNumber(1, 1)
+z.AlgebraToExp()
+print(*z.get())
+z.ExpToAlgebra()
+print(*z.get())
 
