e42af7744287c42eba341951f57ba1f556fa82f8
KristinaChervaneva <chervaneva.kristina@gmail.com>
2022-10-08 19:24:52 +0300

    всякие действия

--- Домашнее задание 3/сложение, вычитание, умножение, деление.py
+++ Домашнее задание 3/сложение, вычитание, умножение, деление.py
@@ -4,7 +4,6 @@ class ComplexNumber:
 
     def __init__(self, my_a = 0, my_b = 0): #инициализатор
         self.set(my_a, my_b)
-        return
 
     def set(self, my_a, my_b): #сеттер
         self.a = my_a
@@ -22,9 +21,26 @@ class ComplexNumber:
         self.a, self.b = self.a * math.cos(self.b), self.a * math.sin(self.b)
         return
 
-    def summa(
+def summa(self, drugoe):
+    return ComplexNumber(self.a + drugoe.a, self.b + drugoe.b)
+
+
+def raznost(self, drugoe):
+    return ComplexNumber(self.a - drugoe.a, self.b - drugoe.b)
+
+
+def proizvedenie(self, drugoe):
+    return ComplexNumber(self.a * drugoe.a - self.b * drugoe.b, self.b * drugoe.a + self.a * drugoe.b)
+
+
+def chastnoe(self, drugoe):
+    return ComplexNumber((self.a * drugoe.a + self.b * drugoe.b) / ((drugoe.a) ** 2 + (drugoe.b) ** 2),
+                    (self.b * drugoe.a - drugoe.b * self.a) / ((drugoe.a) ** 2 + (drugoe.b) ** 2))
 
 
 
 
-z = ComplexNumber(1, 1)
+z1 = ComplexNumber(1, 1)
+z2 = ComplexNumber(2, 2)
+z = proizvedenie(z1, z2)
+print(*z.get())
