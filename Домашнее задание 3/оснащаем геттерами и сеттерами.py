b6b04f8abae054a590ed4c7eefe52d8325a7fd63
KristinaChervaneva <chervaneva.kristina@gmail.com>
2022-10-08 15:14:49 +0300

    ооп

--- Домашнее задание 3/оснащаем геттерами и сеттерами.py
+++ Домашнее задание 3/оснащаем геттерами и сеттерами.py
@@ -1,7 +1,12 @@
 class ComplexNumber:
 
     def __init__(self, my_a = 0, my_b = 0): #инициализатор
+        self.set(my_a, my_b)
+
+    def set(self, my_a, my_b): #сеттер
         self.a = my_a
         self.b = my_b
 
+    def get(self): #геттер
+        return self.a, self.b
 
