849451bbef69c6f0c1cfcd803a7edece2f45db08
KristinaChervaneva <chervaneva.kristina@gmail.com>
2022-12-01 17:32:03 +0300

    вопросы?

---
+++ Домашнее задание 8/Декоратор 1.py
@@ -0,0 +1,16 @@
+def inv_decorator1(func):
+    def wrapper(*args):
+        A = []
+        for i in args:
+            A.append(i)
+        for i in range((len(A))//2):
+            A[i], A[len(A) - i - 1] = A[len(A) - i - 1], A[i]
+        return func(*A)
+    return wrapper
+
+@inv_decorator1
+def foo(*args):
+    return args
+
+
+print(foo(10,7,3,5,10,2,1))
